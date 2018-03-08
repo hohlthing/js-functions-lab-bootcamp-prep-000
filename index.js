3b651e974de136078b6fe9f845eb90cd6c361182
michael hohl <mhohl@rocketmail.com>
2018-03-08 18:50:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function happyHolidayTo(name, holiday)  {
   return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown (days, holiday) {
-  
+  return `It's ${days} days until ${holiday}!`
 }
